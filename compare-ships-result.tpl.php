d79ae1eec7578f5d52dc26eea65625a15d4e048f
rloos289 <rloos289@gmail.com>
2017-03-21 13:41:14 -0700

    initial commit, module has been removed wholesale from the pdxwing site so that it can be made more general

---
+++ compare-ships-result.tpl.php
@@ -0,0 +1,6 @@
+<div class="col-sm-6 ship_result1">
+  <img typeof="foaf:Image" src='sites/default/files/<?php print $ship1; ?>' width="354" height="500">
+</div>
+<div class="col-sm-6 ship_result2">
+  <img typeof="foaf:Image" src='sites/default/files/<?php print $ship2; ?>' width="354" height="500">
+</div>
---
+++ compare-ships.tpl.php
@@ -0,0 +1,7 @@
+<div class="col-sm-6 ship1">
+  <?php print render($form['ship1']); ?>
+</div>
+<div class="col-sm-6 ship2">
+  <?php print render($form['ship2']); ?>
+</div>
+<?php print drupal_render_children($form); ?>
